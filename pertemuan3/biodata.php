e7746b141ee850273e52ee80bf7fc5b496d91236
hibbansm12 <hibbansm12@gmail.com>
2023-09-15 15:38:22 +0700

    pertemuan 3

---
+++ pertemuan3/biodata.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biodata</title>
+    <style>
+        table, tr,td{
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 5px;
+        }
+        h2{padding-left: 60px;}
+    </style>
+</head>
+<body>
+    <h2><?php echo "BIODATA"; ?></h2>
+    <table>
+        <tr>
+            <td><?php echo "Nama"; ?></td>
+            <td>Hibban</td>
+        </tr>
+        <tr>
+            <td><?php echo "Tempat Lahir"; ?></td>
+            <td>Tasikmalaya</td>
+        </tr>
+        <tr>
+            <td><?php echo "Tanggal Lahir"; ?></td>
+            <td>17 Desember 2003</td>
+        </tr>
+        <tr>
+            <td><?php echo "Hobi"; ?></td>
+            <td>Main game</td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ pertemuan3/index.php
@@ -0,0 +1,32 @@
+<?php
+//Ini komentar
+/*Ini komentar
+lorem */
+// disini ada komentar
+echo "Hibban  ";
+print "Sani ";
+print_r("Muttaqin "); //untuk array
+var_dump(12);
+var_dump(2<=3, 5<4, 1!=2);
+var_dump(2>3||3>1);
+echo "<br>";
+$nama = "Hibban";
+echo $nama;
+$x=3;
+$y=4;
+echo "<br>".$x+$y;
+$a="Hello ";
+$b="World";
+echo "<br>".$a.$b;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="font-family: Arial, Helvetica, sans-serif;"><?php echo "ini php"; ?></h1>
+</body>
+</html>
